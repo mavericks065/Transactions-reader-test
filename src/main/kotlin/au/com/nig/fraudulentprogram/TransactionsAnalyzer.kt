7a7d65401bb221b0e3cce6ab69de8832755148e1
nicolas <nicolas065@gmail.com>
2021-04-29 06:28:13 +1000

    Acceptance tests that should validate the business of the program

---
+++ src/main/kotlin/au/com/nig/fraudulentprogram/TransactionsAnalyzer.kt
@@ -0,0 +1,7 @@
+package au.com.nig.fraudulentprogram
+
+import java.math.BigDecimal
+
+object TransactionsAnalyzer {
+    fun result(filePath: String, threshold: BigDecimal): String = TODO()
+}
---
+++ src/test/kotlin/au/com/nig/fraudulentprogram/TransactionsAnalyzerTest.kt
@@ -0,0 +1,58 @@
+package au.com.nig.fraudulentprogram
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import java.math.BigDecimal
+import java.nio.file.Path
+import java.nio.file.Paths
+
+object TransactionsAnalyzerTest: Spek({
+    group("when reading a file and performing an analysis"){
+        describe("without any fraudulent activity") {
+            it("should tell us the activity is clean") {
+                // Given
+                val currentRelativePath: Path = Paths.get("")
+                val s = currentRelativePath.toAbsolutePath().toString()
+                val filePath = "$s/src/test/resources/short_sample.csv"
+                val threshold = BigDecimal.valueOf(1000.0)
+
+                // When
+                val result = TransactionsAnalyzer.result(filePath, threshold)
+
+                // Then
+                assertEquals("No Fraudulent activity", result)
+            }
+        }
+        describe("with fraudulent activity") {
+            it("should send us back the hash of the credit card number") {
+                // Given
+                val currentRelativePath: Path = Paths.get("")
+                val s = currentRelativePath.toAbsolutePath().toString()
+                val filePath = "$s/src/test/resources/short_sample.csv"
+                val threshold = BigDecimal.valueOf(40.0)
+
+                // When
+                val result = TransactionsAnalyzer.result(filePath, threshold)
+
+                // Then
+                assertEquals("10d7ce2f43e35fa57d1bbf8b1e2", result)
+            }
+            it("should send us back the hash of the credit card number even if activity is fraudulent only over a sliding period") {
+                // Given
+                val currentRelativePath: Path = Paths.get("")
+                val s = currentRelativePath.toAbsolutePath().toString()
+                val filePath = "$s/src/test/resources/sample.csv"
+                val threshold = BigDecimal.valueOf(150.0)
+
+                // When
+                val result = TransactionsAnalyzer.result(filePath, threshold)
+
+                // Then
+                assertEquals("10d7ce2f43e35fa57d1bbf8b1e2", result)
+            }
+        }
+
+    }
+})
